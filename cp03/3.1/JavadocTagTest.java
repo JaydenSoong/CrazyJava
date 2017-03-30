47b266bbecc87298dc23fe2f5f0ccd32b98249a8
JaydenSoong <jiandongsong@126.com>
2017-03-31 00:16:35 +0800

    Add JavadocTagTest.java

---
+++ cp03/3.1/JavadocTagTest.java
@@ -0,0 +1,22 @@
+package jayden;
+
+/**
+ * Description:
+ * <br>网站：<a href = "http://www.baidu.com">百度</a>
+ * <br>Copyright (C), 2001-2017, Jayden.Soong
+ * <br>This program is protected by copyright laws.
+ * <br>Program Name:
+ * <br>Date:
+ * @author Jayden.Soong jiandongsong@126.com
+ * @version 1.0
+ */
+public class JavadocTagTest{
+    /**
+     * 一个得到打招呼字符串的方法
+     * @param name 该参数指定向谁打招呼
+     * @return 返回打招呼的字符串
+     */
+    public String hello(String name){
+        return name + "，你好！";
+    }
+}
