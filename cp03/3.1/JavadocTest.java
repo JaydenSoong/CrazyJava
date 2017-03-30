b4acb4c4b0113cde2e6e9770c9c522e68e638c10
JaydenSoong <jiandongsong@126.com>
2017-03-31 00:15:08 +0800

    Add JavadocTest.java

---
+++ cp03/3.1/JavadocTest.java
@@ -0,0 +1,24 @@
+package soong;
+
+/**
+ * Description:
+ * <br>网站：<a href = "http://www.baidu.com">百度</a>
+ * <br>Copyright (C), 2001-2017, Jayden.Soong
+ * <br>This program is protected by copyright laws.
+ * <br>Program Name:
+ * <br>Date:
+ * @author Jayden.Soong jiandongsong@126.com
+ * @version 1.0
+ */
+public class JavadocTest{
+    /**
+     * 简单测试成员变量
+     */
+    protected String name;
+    /**
+     * 主方法，程序的入口
+     */
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+    }
+}
