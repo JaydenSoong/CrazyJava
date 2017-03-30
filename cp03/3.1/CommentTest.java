6b9138548130b886ea628dc59577f39e368fe22e
JaydenSoong <jiandongsong@126.com>
2017-03-31 00:13:44 +0800

    add CommentTest.java

---
+++ cp03/3.1/CommentTest.java
@@ -0,0 +1,11 @@
+public class CommentTest{
+    /*
+     * 这是一个多行注释
+     * 我要开始学 Java 了。
+     */
+    public static void main(String[] args){
+        //这是一行单行注释
+        System.out.println("Hello, World!");
+        //System.out.println("这行将被单行注释");
+    }
+}
