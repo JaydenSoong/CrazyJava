439cc0b7152642a83d041fd3d99e54c5467e00ba
JaydenSoong <jiandongsong@126.com>
2017-03-31 00:15:46 +0800

    Add Test.java

---
+++ cp03/3.1/Test.java
@@ -0,0 +1,23 @@
+package jayden;
+
+/**
+ * Description:
+ * <br>网站：<a href = "http://www.baidu.com">百度</a>
+ * <br>Copyright (C), 2001-2017, Jayden.Soong
+ * <br>This program is protected by copyright laws.
+ * <br>Program Name:
+ * <br>Date:
+ * @author Jayden.Soong jiandongsong@126.com
+ * @version 1.0
+ */
+public class Test{
+    /**
+     * 简单测试成员变量
+     */
+    public int age;
+    /**
+     * 主方法，程序的入口
+     */
+    public Test(){
+    }
+}
