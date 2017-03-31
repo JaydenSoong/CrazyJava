9d88737ab094ee31af2082996518b3d5ac82d318
JaydenSoong <jiandongsong@126.com>
2017-03-31 21:37:59 +0800

    Add UnderscoreTest.java

---
+++ cp03/3.4/UnderscoreTest.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class UnderscoreTest{
+    public static void main(String[] args){
+        //定义一个 32 位的二进制数
+        int binValue = 0B1000_0000_0000_0000_0000_0000_0000_0011; 
+        //定义 PI
+        double PI = 3.141_592_653;
+        //定义一个 double
+        double height = 8_8_4_8.23;
+        System.out.println("binValue = " + binValue);
+        System.out.println("PI = " + PI);
+        System.out.println("height = " + height);
+    }
+}
