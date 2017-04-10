a948cf06202834493db705c23f12d8648e67f7d4
JaydenSoong <jiandongsong@126.com>
2017-04-10 23:58:01 +0800

    Add DeadForTest.java

---
+++ cp04/4.3/DeadForTest.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class DeadForTest{
+
+    public static void main(String[] args){
+        // 省略了 for 循环的三个部分，循环条件将一直为 true
+        for( ; ; ){
+            System.out.println("++++++++++++");
+        }
+    }
+}
