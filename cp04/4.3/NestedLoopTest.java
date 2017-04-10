d57031b3bef49d19e70d7ed11e8364698685522b
JaydenSoong <jiandongsong@126.com>
2017-04-11 00:12:38 +0800

    Add NestedLoopTest.java

---
+++ cp04/4.3/NestedLoopTest.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class NestedLoopTest{
+
+    public static void main(String[] args){
+        // 外层循环
+        for(int i = 0; i < 5; i++){
+            // 内层循环
+            for(int j = 0; j < 3; j++){
+                System.out.println("i = " + i + "; j = " + j);
+            }
+        }
+    }
+}
