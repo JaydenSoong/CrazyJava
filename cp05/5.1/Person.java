95b2695efeabe071480a75cfa31328de85c33567
JaydenSoong <jiandongsong@126.com>
2017-04-13 22:24:19 +0800

    Add Person.java

---
+++ cp05/5.1/Person.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Person{
+    // 下面定义了两个成员变量
+    public String name;
+    public int age;
+
+    // 下面定义了一个 say 方法
+    public void say(String content){
+        System.out.println(content);
+    }
+}
