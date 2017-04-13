ee3f42279c88382d064f79f63c0f11e2abed09c2
JaydenSoong <jiandongsong@126.com>
2017-04-13 22:38:43 +0800

    Add PersonTest.java

---
+++ cp05/5.1/PersonTest.java
@@ -0,0 +1,26 @@
+package classes;
+
+public class PersonTest{
+    
+    public static void main(String[] args){
+        
+        // 使用 Person 类定义一个变量
+        Person p;
+
+        // 使用 Person 类创建一个实例，并将其赋值给之前创建的变量
+        p = new Person();
+
+        // 上面两个语句可以简写成如下形式
+        // Person p = new Person();
+
+        // 访问 p 的 name 成员变量，并赋值
+        p.name = "李刚";
+
+        // 访问 p 的方法
+        p.say("Java 语言很简单，学习很容易");
+
+        // 直接输出 p 的 name 成员变量
+        System.out.println(p.name);
+    }
+    
+}
