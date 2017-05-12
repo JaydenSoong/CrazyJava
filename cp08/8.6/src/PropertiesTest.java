c0ea72787788643f577ea964e5fbe8768468c817
JaydenSoong <jiandongsong@126.com>
2017-05-12 22:41:46 +0800

    add storeToXML loadFromXML

--- cp08/8.6/src/PropertiesTest.java
+++ cp08/8.6/src/PropertiesTest.java
@@ -13,11 +13,15 @@ public class PropertiesTest {
         prop1.setProperty("email", "jiandongsong@126.com");
         // 将属性集输出到文件 a.ini
         prop1.store(new FileOutputStream("a.ini"), "comment lines");
+        // 存成 XML 格式
+        prop1.storeToXML(new FileOutputStream("a.xml"), "comment lines");
 
         Properties prop2 = new Properties();
         prop2.setProperty("gender", "male");
         // 加载属性集文件 a.ini，并将 prop2 中设置的属性追加到属性集文件的最后
-        prop2.load(new FileInputStream("a.ini"));
+        // prop2.load(new FileInputStream("a.ini"));
+        // 加载属性集文件 a.xml，并将 prop2 中设置的属性追加到属性集文件的最后
+        prop2.loadFromXML(new FileInputStream("a.xml"));
         System.out.println(prop2);
     }
 }
