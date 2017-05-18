4b1a8e74c46b76d7a362bd4e52c11d0a34750147
JaydenSoong <jiandongsong@126.com>
2017-05-18 21:55:56 +0800

    Java 的异常跟踪栈

---
+++ cp10/10.5/src/PrintStackTraceTest.java
@@ -0,0 +1,27 @@
+package classes;
+
+class SelfException extends RuntimeException {
+    public SelfException(){}
+
+    public SelfException(String msg) {
+        super(msg);
+    }
+}
+
+public class PrintStackTraceTest {
+    public static void main (String[] args) {
+        firstMethod();    
+    }
+
+    public static void firstMethod() {
+        secondMethod();
+    }
+
+    public static void secondMethod() {
+        thirdMethod();
+    }
+
+    public static void thirdMethod() {
+        throw new SelfException("自定义异常信息");
+    }
+}
