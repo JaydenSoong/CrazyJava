43406975881eca6358280fc63129986daeb99570
JaydenSoong <jiandongsong@126.com>
2017-06-19 17:05:31 +0800

    实现 Runnable 接口，使用 Lambda 表达式创建线程类

---
+++ cp16_multi_thread/lesson_02/src/SecondThreadUseLambda.java
@@ -0,0 +1,23 @@
+/**
+ * Created by soong on 17-6-19.
+ * 实现 Runnable 接口，使用 Lambda 表达式创建线程类
+ */
+public class SecondThreadUseLambda{
+    private static int j;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+
+            if (i == 20) {
+                Runnable r = ()-> {
+                    for (; j < 100; j++) {
+                        System.out.println(Thread.currentThread().getName() + " " + j);
+                    }
+                };
+                //通过 new Thread(target, name) 方法创建新线程
+                new Thread(r, "新线程").start();
+            }
+        }
+    }
+}
